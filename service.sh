760acb2ee025a20dd93b0543b38e5063be0baa38
readonlynux <readonlynux@proton.me>
2026-05-28 20:36:10 +0300

    Update service.sh

---
+++ service.sh
@@ -0,0 +1,13 @@
+MOD_PATH="${0%/*}"
+
+[ ! -f "$MOD_PATH/bootanim.sh" ] && exit 1
+
+if [ "$KSU" = "true" ]; then
+	ln -sf "$MOD_PATH/bootanim.sh" "/data/adb/ksu/bin/bootanim"
+elif [ "$APATCH" = "true" ]; then
+	ln -sf "$MOD_PATH/bootanim.sh" "/data/adb/ap/bootanim"
+elif [ "$MAGISK" = "true" ]; then
+	[ -w /sbin ] && magisktmp=/sbin
+	[ -w /debug_ramdisk ] && magisktmp=/debug_ramdisk
+	ln -sf "$MOD_PATH/bootanim.sh" "$magisktmp/bootanim"
+fi
