fe3bc71a8a367a63a166453317bf08b4229c248c
readonlynux <readonlynux@proton.me>
2026-05-28 20:36:48 +0300

    Update customize.sh

---
+++ customize.sh
@@ -0,0 +1,16 @@
+persist_dir="/data/adb/bootanimation"
+
+ui_print '- Installing Bootanimation Changer without OverlayFS'
+chmod +x $MODPATH/*.sh
+for i in "/product/media/bootanimation.zip" "/system/product/media/bootanimation.zip" "/system/media/bootanimation.zip" "/oem/media/bootanimation.zip"; do
+	if [ -f "$i" ]; then
+		TARGET_PATH="$i"
+		break
+	fi
+done
+if [ -n "$TARGET_PATH" ]; then
+	mkdir -p "$persist_dir"
+	echo "$TARGET_PATH" >"$persist_dir/bootanimation_path.txt"
+else
+	abort '! Bootanimation file was not found in the system path'
+fi
