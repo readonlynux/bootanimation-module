cfef08b3001062a3cbb52c7f1bf70dbd9d2a5519
readonlynux <readonlynux@proton.me>
2026-06-16 20:05:26 +0300

    feat(post-fs-data.sh): skip bootanimation if disable tag exists

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,7 +1,9 @@
-bootani_file=/data/adb/bootanimation/bootanimation.zip
+persist_dir=/data/adb/bootanimation
+bootani_file=$persist_dir/bootanimation.zip
 [ ! -f "/data/adb/bootanimation/bootanimation_path.txt" ] && exit 1
 system_path="$(cat "/data/adb/bootanimation/bootanimation_path.txt")"
 
+[ -f "$persist_dir/disable" ] && exit
 if [ ! -f "$bootani_file" ]; then
 	exit 1
 elif [ -f "$bootani_file" ]; then
