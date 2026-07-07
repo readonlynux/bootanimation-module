7f98ec5ac5bccdc2005014e77da400fa7bbbe089
RONUX <readonlynux@proton.me>
2026-07-07 12:32:51 +0300

    Update version in help menu to v1.0-stable

--- bootanim.sh
+++ bootanim.sh
@@ -8,7 +8,7 @@ user_anipath="$2"
 # ======== functions ========
 help_menu(){
 	echo '==================================================='
-	echo 'bootanim <command> [arg]                 v1.0.alpha'
+	echo 'bootanim <command> [arg]                v1.0-stable'
 	echo '==================================================='
 	echo 'set       Apply bootanimation.zip'
 	echo 'enable    Enable custom bootanimation'
