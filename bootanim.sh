ed72301103a21b4bba20329510ac59da6f57254d
readonlynux <readonlynux@proton.me>
2026-07-15 16:26:25 +0300

    refactor(bootanim.sh): add shebang and add create filesystem path file conditions

--- bootanim.sh
+++ bootanim.sh
@@ -1,3 +1,5 @@
+#!/system/bin/sh
+
 # ======== variables ========
 persist_dir=/data/adb/bootanimation
 bootani_path=$persist_dir/bootanimation.zip
@@ -33,6 +35,10 @@ log_msg(){
 if [ "$user_command" = 'set' ]; then
 	[ ! -f "$user_anipath" ] && abort_msg 'Bootanimation file not found!'
 	mkdir -p "$persist_dir"
+	if [ ! -f "$persist_dir/bootanimation_file.txt" ]; then
+		$MODDIR/create_bootanimation_filepath.sh
+		[ "$?" = "404" ] && abort_msg "Bootanimation file was not found in the system path"
+	fi
 	cp -f "$user_anipath" "$bootani_path"
 	chcon "u:object_r:system_file:s0" "$bootani_path"
 	chown root:root "$bootani_path"
