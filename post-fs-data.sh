0abb189c341d0772478f362d97616895f945d0cb
readonlynux <readonlynux@proton.me>
2026-07-15 15:49:56 +0300

    refactor(post-fs-data.sh): improvements

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,13 +1,17 @@
+MODDIR=${0%/*}
 persist_dir=/data/adb/bootanimation
 bootani_file=$persist_dir/bootanimation.zip
-[ ! -f "/data/adb/bootanimation/bootanimation_path.txt" ] && exit 1
-system_path="$(cat "/data/adb/bootanimation/bootanimation_path.txt")"
+[ ! -f "$persist_dir/bootanimation_path.txt" ] && exit 1
+system_path="$(cat "$persist_dir/bootanimation_path.txt")"
 
 [ -f "$persist_dir/disable" ] && exit
 if [ ! -f "$bootani_file" ]; then
 	exit 1
 elif [ -f "$bootani_file" ]; then
-	[ ! -f "$system_path" ] && exit 1
+	if [ ! -f "$system_path" ]; then
+		[ -f "$persist_dir/bootanimation_path.txt" ] && rm "$persist_dir/bootanimation_path.txt"
+		$MODDIR/create_bootanimation_pathfile.sh
+	fi
 	umount -l "$system_path"
 	mount --bind "$bootani_file" "$system_path"
 fi
