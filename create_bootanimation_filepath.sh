57b0a794570bc9fab2ae21c3fbea80c4f4cb9579
readonlynux <readonlynux@proton.me>
2026-07-15 15:59:54 +0300

    feat(create_bootanimation_filepath.sh): check filesystem and create new path file

---
+++ create_bootanimation_filepath.sh
@@ -0,0 +1,15 @@
+#!/system/bin/sh
+persist_dir=/data/adb/bootanimation
+
+for i in "/product/media/bootanimation.zip" "/system/product/media/bootanimation.zip" "/system/media/bootanimation.zip" "/oem/media/bootanimation.zip"; do
+	if [ -f "$i" ]; then
+		TARGET_PATH="$i"
+		break
+	fi
+done
+if [ -n "$TARGET_PATH" ]; then
+	mkdir -p "$persist_dir"
+	echo "$TARGET_PATH" >"$persist_dir/bootanimation_path.txt"
+else
+	exit 1
+fi
